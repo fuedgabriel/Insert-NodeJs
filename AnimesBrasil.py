a7ea64a66748ba419b114da8cf2b078fc6308485
Fued Gabriel <fuedgabriel@hotmail.com>
2020-02-27 23:52:55 -0300

    Update AnimesBrasil.py

--- AnimesBrasil.py
+++ AnimesBrasil.py
@@ -17,14 +17,43 @@ videos_titleHD = []
 videos_urlHD = []
 videos_mp4HD = []
 
+Year = ["Null"]
+EpisodesLEG = [0]
+EpisodesDUB = [0]
+EpisodeLEG = [0]
+EpisodeDUB = [0]
+Ovass = [0]
+Movies = [0]
+Quality = ["Null"]
+Category = ["Null"]
+Anime = ["null"]
+Anime_id = ["Null"]
+DownloadBG = ["Null"]
+DownloadSD = ["Null"]
+DownloadHD = ["Null"]
+ErrosPage = ["Null"]
+PageLocal = ["Null"]
 
-def Control(Anime, Anime_id, EpisodeLEG, EpisodeDUB, EpisodesLEG, EpisodesDUB, Quality, Category):
-    print("***************-----PAINEL DE CONTROLE***************-----")
-    print("Nome: "+str(Anime) + "           Id: "+str(Anime_id))
-    print("Legendado: "+str(EpisodeLEG)+" de "+str(EpisodesLEG))
-    print("Dublado: "+str(EpisodeDUB)+" de "+str(EpisodesDUB))
-    print("Categorias: " +
-          str(Category).replace('[', '').replace(']', '').replace('\'', ''))
+
+def Control():
+    while True:
+        time.sleep(2)
+        print(
+            "***************-----PAINEL DE CONTROLE***************----- Página: "+str(PageLocal[0]))
+        print("Nome: "+str(Anime[0])+"           Id: " +
+              str(Anime_id[0])+"   Ano: \033[31m" + str(Year[0])+'\033[0;0m')
+        print("Legendado: "+str(EpisodesLEG[0])+" de "+str(EpisodeLEG[0]))
+        print("Dublado: "+str(EpisodesDUB[0])+" de "+str(EpisodeDUB[0]))
+        print("Qualidades --> BG: " +
+              DownloadBG[0]+"   SD: "+DownloadSD[0]+"   HD: "+DownloadHD[0])
+        print("Ovas: " +
+              str(Ovass).replace('[', '').replace(']', '').replace('\'', ''))
+        print("Filmes: " +
+              str(Movies).replace('[', '').replace(']', '').replace('\'', ''))
+        print("Categorias: " +
+              str(Category).replace('[', '').replace(']', '').replace('\'', ''))
+        print("Erros páginas: " +
+              str(ErrosPage).replace('[', '').replace(']', '').replace('\'', ''))
 
 
 def clear():
@@ -74,16 +103,18 @@ def GetPage():
 
 def GetAllAnimes():
     page = str(GetPage())
-    print(int(page[:-3])+1)
+    # print(int(page[:-3])+1)
     for x in range(1, int(page[:-3])+1):
+        PageLocal.clear()
+        PageLocal.append(x)
 
-        print()
-        print()
-        print('*****************************')
-        print('Página '+str(x))
-        print('*****************************')
-        print()
-        print()
+        # print()
+        # print()
+        # print('*****************************')
+        # print('Página '+str(x))
+        # print('*****************************')
+        # print()
+        # print()
         try:
             url = 'https://remainder.myvideo.vip/api-new/animes/' + \
                 str(x)+'?search=all'
@@ -91,30 +122,32 @@ def GetAllAnimes():
             ListAnimes = json.loads(ListAnimes.content)
             if(ListAnimes == "{\"animes\":{}}"):
                 insertNoID('pages', x, url)
-                print('___________________________')
-                print('Erro na página: '+str(x))
-                print('___________________________')
+                ErrosPage.append(x)
+                # print('___________________________')
+                # print('Erro na página: '+str(x))
+                # print('___________________________')
             else:
                 ListAnimes = ListAnimes['animes']['animes']
                 for y in range(len(ListAnimes)):
-                    print('id: '+str(ListAnimes[y]['id']))
-                    print('Nome: '+str(ListAnimes[y]['nome']))
-                    print('Numero da temporada: ' +
-                          str(ListAnimes[y]['numTemporada']))
-                    print('Temporada: '+str(ListAnimes[y]['temporada']))
-                    print('Imagem: '+str(ListAnimes[y]['capa']))
-                    print('_________________________________________________')
-                    print()
-                    print()
+                    # print('id: '+str(ListAnimes[y]['id']))
+                    # print('Nome: '+str(ListAnimes[y]['nome']))
+                    # print('Numero da temporada: ' +
+                    #       str(ListAnimes[y]['numTemporada']))
+                    # print('Temporada: '+str(ListAnimes[y]['temporada']))
+                    # print('Imagem: '+str(ListAnimes[y]['capa']))
+                    # print('_________________________________________________')
+                    # print()
+                    # print()
                     os.system('mkdir '+str(ListAnimes[y]['id']))
                     GetInfo(str(ListAnimes[y]['id']), ListAnimes[y]['nome'])
         except:
+            ErrosPage.append(x)
             insertNoID('página', x, url)
-            print('___________________________')
-            print('Erro na página: '+str(x))
-            print('___________________________')
-            print()
-            print()
+            # print('___________________________')
+            # print('Erro na página: '+str(x))
+            # print('___________________________')
+            # print()
+            # print()
 
 
 def GetInfo(id, name):
@@ -153,28 +186,47 @@ def GetInfo(id, name):
                 ovas.append(z['nome'])
                 ovas_id.append(z['id'])
                 ovas_img.append(z['capa'])
-            Control(name, id, ep_leg, ep_dub, 0, 0, 'SD', category)
-            input()
+            Year.clear()
+            Anime_id.clear()
+            EpisodeDUB.clear()
+            EpisodeLEG.clear()
+            Category.clear()
+            Anime.clear()
+            Ovass.clear()
+            Movies.clear()
+            EpisodesDUB.clear()
+            EpisodesLEG.clear()
+
+            EpisodesLEG.append(0)
+            EpisodesDUB.append(0)
+            Movies.append(len(id_movie))
+            Ovass.append(len(ovas_id))
+            Anime.append(name)
+            Anime_id.append(id)
+            EpisodeLEG.append(ep_leg)
+            EpisodeDUB.append(ep_dub)
+            Year.append(year)
+            Category.append(category)
             if(dub == True):
-                print()
-                print('____________________________________________')
-                print('Episódios dublado')
+                # print()
+                # print('____________________________________________')
+                # print('Episódios dublado')
                 GetEp(_id, '1', False, 'DUB')
-                print('____________________________________________')
-                print()
-                print()
-                print()
-                print()
+                # print('____________________________________________')
+                # print()
+                # print()
+                # print()
+                # print()
 
             if(leg == True):
-                print()
-                print('____________________________________________')
-                print('Episódios legendado')
+                # print()
+                # print('____________________________________________')
+                # print('Episódios legendado')
                 GetEp(_id, '1', False, 'LEG')
-                print('____________________________________________')
-                print()
-                print()
-                print()
+                # print('____________________________________________')
+                # print()
+                # print()
+                # print()
                 # input()
         else:
             insert('Anime', id, id, url)
@@ -201,7 +253,7 @@ def GetEp(id, page, validator, language):
     if(validator == True):
         eps = eps['eps']
         for x in eps:
-            print(x)
+            # print(x)
             if(x['link_bg'] == True):
                 tLINKbg = threading.Thread(target=GetVideoBG, args=(x['id'],))
                 tLINKbg.start()
@@ -211,7 +263,7 @@ def GetEp(id, page, validator, language):
             if(x['link_sd'] == True):
                 tLINKsd = threading.Thread(target=GetVideoSD, args=(x['id'],))
                 tLINKsd.start()
-            print('_____________________')
+            # print('_____________________')
         return 0
 
     if(eps['paginas'] > 1.9):
@@ -233,19 +285,40 @@ def GetEp(id, page, validator, language):
     tHD.start()
     while True:
         if(tSD.is_alive() == False and tBG.is_alive() == False and tHD.is_alive() == False):
-            print('\nDownload finalizado')
-        #     tBG._stop()
-        #     tSD._stop()
-        #     tHD._stop()
-        #    tLINKhd._stop()
-        #     tLINKsd._stop()
-        #     tLINKbg._stop()
+            DownloadBG.clear()
+            DownloadSD.clear()
+            DownloadHD.clear()
+            DownloadBG.append("Finalizado")
+            DownloadSD.append("Finalizado")
+            DownloadHD.append("Finalizado")
+            # print('\nDownload finalizado')
             break
         else:
-            print('\nBG: '+str(tBG.is_alive()))
-            print('SD: '+str(tSD.is_alive()))
-            print('HD: '+str(tHD.is_alive()))
-            print('Download em andamento')
+            # print('\nBG: '+str(tBG.is_alive()))
+            # print('SD: '+str(tSD.is_alive()))
+            # print('HD: '+str(tHD.is_alive()))
+            # print('Download em andamento')
+            if(tBG.is_alive() == True):
+                DownloadBG.clear()
+                DownloadBG.append("Em andamento")
+            else:
+                DownloadBG.clear()
+                DownloadBG.append("Finalizado")
+
+            if(tSD.is_alive() == True):
+                DownloadSD.clear()
+                DownloadSD.append("Em andamento")
+            else:
+                DownloadSD.clear()
+                DownloadSD.append("Finalizado")
+
+            if(tHD.is_alive() == True):
+                DownloadHD.clear()
+                DownloadHD.append("Em andamento")
+            else:
+                DownloadHD.clear()
+                DownloadHD.append("Finalizado")
+
             time.sleep(3)
     clear()
 
@@ -290,18 +363,25 @@ def GetVideoHD(id_ep):
 
 
 def DownloadVideo(url, title, mp4, _id, quality, language):
-
     if(url == []):
         return False
     os.system('cd '+str(_id)+' && mkdir '+quality)
     # print(url)
-    print('Iniciando download')
+    # print('Iniciando download')
     zz = 0
     for x in url:
         zz = zz+1
-        print('URL: {'+str(x) + '}')
+        if(language == 'LEG'):
+            EpisodesLEG.clear()
+            EpisodesLEG.append(zz)
+        else:
+            EpisodesDUB.clear()
+            EpisodesDUB.append(zz)
+
+        # print('URL: {'+str(x) + '}')
         try:
-            print("Download concluido :"+str(x))
+            time.sleep(3)
+            # print("Download concluido :"+str(x))
             # responsee = requests.head(x, allow_redirects=True)
             # wget.download(responsee.url)
         except:
@@ -309,4 +389,5 @@ def DownloadVideo(url, title, mp4, _id, quality, language):
 
 
 # GetEp(str(12), '1', False, 'LEG')
+threading._start_new_thread(Control)
 GetAllAnimes()
